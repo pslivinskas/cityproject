e643b659aa39b3c61cb3a7bfab13271d145c2098
pslivinskas <povilaslsivinskas@gmail.com>
2023-01-18 14:22:47 +0200

    Initial commit

---
+++ src/main/java/lt/povilas/cityproject/CityprojectApplication.java
@@ -0,0 +1,13 @@
+package lt.povilas.cityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityprojectApplication.class, args);
+	}
+
+}
